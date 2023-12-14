b3f68e37ade05e0a8a80570d11c014aaf1e90ac3
Richard Weerasinghe <richard@skyscrapr.io>
2023-12-15 09:33:38 +1300

    add projects datasource

---
+++ examples/data-sources/zilliz_projects/data-source.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    zilliz = {
+      source = "skyscrapr/zilliz"
+    }
+  }
+}
+
+provider "zilliz" {
+  cloud_region_id = "gcp-us-west1"
+}
+
+data "zilliz_projects" "example" {}
